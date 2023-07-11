b8816fb4bc1ad91c98f6a1360a48e7860c67e7eb
christopher <christopher@bioinf.uni-leipzig.de>
2023-07-11 16:14:36 +0200

    Begin (sphinx) Manual

--- Manual/docs/source/conf.py
+++ Manual/docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.5'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['rst2pdf.pdfbuilder']
 
 templates_path = ['_templates']
 exclude_patterns = []
