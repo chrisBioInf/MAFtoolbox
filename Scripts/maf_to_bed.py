5396184175aa5245e2ccf2e5e94d886e3a387cae
christopher <christopher@bioinf.uni-leipzig.de>
2023-07-25 12:13:16 +0200

    Add clustering function for genome index coordinates

--- Scripts/maf_to_bed.py
+++ Scripts/maf_to_bed.py
@@ -10,7 +10,13 @@ Created on Thu Feb  9 09:03:11 2023
 from utility import read_maf, check_positional_argument, strand_dict
 
 
-def maf_to_bed(fs, output):
+def coordinate_overlap(end, ref_start, ref_end):
+    if (end >= ref_start) and (end <= ref_end):
+        return True
+    return False
+
+
+def maf_to_bed(fs, output, cluster):
     seqname = []
     starts = []
     ends = []
@@ -20,15 +26,43 @@ def maf_to_bed(fs, output):
     alignment_handle = read_maf(fs)
     count = 0
     
-    for alignment in alignment_handle:
-        count += 1
+    if cluster == False:
+        for alignment in alignment_handle:
+            count += 1
+            record = alignment[0]
+            seqname.append(record.id)
+            starts.append(record.annotations.get("start"))
+            ends.append(record.annotations.get("start") + record.annotations.get("size"))
+            score.append(".")
+            strands.append(strand_dict.get(record.annotations.get("strand")))
+    
+    else:
+        alignment = next(alignment_handle)
         record = alignment[0]
         seqname.append(record.id)
         starts.append(record.annotations.get("start"))
         ends.append(record.annotations.get("start") + record.annotations.get("size"))
         score.append(".")
         strands.append(strand_dict.get(record.annotations.get("strand")))
+        count += 1
         
+        for alignment in alignment_handle:
+            record = alignment[0]
+            start = record.annotations.get("start")
+            end = record.annotations.get("start") + record.annotations.get("size")
+            strand = strand_dict.get(record.annotations.get("strand"))
+            
+            if (record.id == seqname[-1]) and (strand == strands[-1]) and coordinate_overlap(ends[-1], start, end):
+                ends[-1] = end
+                continue
+            
+            seqname.append(record.id)
+            starts.append(start)
+            ends.append(end)
+            score.append(".")
+            strands.append(strand)
+            count += 1
+    
     if output == "":
         for i in range(0, count):
             line = "%s\t%s\t%s\t%s\t%s" % (
@@ -47,11 +81,11 @@ def maf_to_bed(fs, output):
 
 
 def write_to_bed(parser):
-    parser.add_option("-i","--input",action="store",type="string", dest="input",help="The (MAF) input file (Required).")
     parser.add_option("-o","--output",action="store",type="string", default="", dest="out_file",help="BED file to write to. If empty, coordinates are redirected to stdout.")
+    parser.add_option("-c", "--cluster",action="store_true",default=False,dest="cluster",help="If set, overlapping and same-stranded coordinates will be merged (Default: False).")
     options, args = parser.parse_args()
     args = args[1:]
     handle_ = check_positional_argument(args)
     
-    maf_to_bed(handle_, output=options.out_file)
+    maf_to_bed(handle_, output=options.out_file, cluster=options.cluster)
 
--- Scripts/maf_to_gtf.py
+++ Scripts/maf_to_gtf.py
@@ -7,10 +7,11 @@ Created on Thu Feb  9 09:03:11 2023
 """
 
 
+from maf_to_bed import coordinate_overlap
 from utility import read_maf, check_positional_argument, strand_dict
 
 
-def maf_to_gtf(fs, output):
+def maf_to_gtf(fs, output, cluster):
     seqname = []
     source = []
     type_ = []
@@ -20,27 +21,60 @@ def maf_to_gtf(fs, output):
     strands = []
     frame = []
     descriptor = []
-    
-    if "window" in fs:
-        this_source = "rnazWindow"
-    else:
-        this_source = "MAF"
+
+    this_source = "MAF"
     
     alignment_handle = read_maf(fs)
     count = 0
     
-    for alignment in alignment_handle:
-        count += 1
+    if cluster == False:
+        for alignment in alignment_handle:
+            count += 1
+            record = alignment[0]
+            seqname.append(record.id)
+            source.append(this_source)
+            type_.append("alignment_block")
+            starts.append(record.annotations.get("start"))
+            ends.append(record.annotations.get("start") + record.annotations.get("size"))
+            score.append(".")
+            frame.append(".")
+            strands.append(strand_dict.get(record.annotations.get("strand")))
+            descriptor.append("Continuously aligned block %s" % count)
+            
+    else:
+        alignment = next(alignment_handle)
         record = alignment[0]
         seqname.append(record.id)
-        source.append(this_source)
         type_.append("alignment_block")
+        source.append(this_source)
         starts.append(record.annotations.get("start"))
         ends.append(record.annotations.get("start") + record.annotations.get("size"))
         score.append(".")
         frame.append(".")
         strands.append(strand_dict.get(record.annotations.get("strand")))
         descriptor.append("Continuously aligned block %s" % count)
+        count += 1
+        
+        for alignment in alignment_handle:
+            record = alignment[0]
+            start = record.annotations.get("start")
+            end = record.annotations.get("start") + record.annotations.get("size")
+            strand = strand_dict.get(record.annotations.get("strand"))
+            
+            if (record.id == seqname[-1]) and (strand == strands[-1]) and coordinate_overlap(ends[-1], start, end):
+                ends[-1] = end
+                continue
+            
+            seqname.append(record.id)
+            type_.append("alignment_block")
+            source.append(this_source)
+            starts.append(start)
+            ends.append(end)
+            frame.append(".")
+            score.append(".")
+            strands.append(strand)
+            descriptor.append("Continuously aligned block %s" % count)
+            count += 1
         
     if output == "":
         for i in range(0, count):
@@ -61,9 +95,10 @@ def maf_to_gtf(fs, output):
 
 def write_to_gtf(parser):
     parser.add_option("-o","--output",action="store",type="string", default="", dest="out_file",help="GTF file to write to. If empty, coordinates are redirected to stdout.")
+    parser.add_option("-c", "--cluster",action="store_true",default=False,dest="cluster",help="If set, overlapping and same-stranded coordinates will be merged (Default: False).")
     options, args = parser.parse_args()
     args = args[1:]
     handle_ = check_positional_argument(args)
     
-    maf_to_gtf(handle_, output=options.out_file)
+    maf_to_gtf(handle_, output=options.out_file, cluster=options.cluster)
 
